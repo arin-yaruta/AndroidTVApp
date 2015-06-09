93ce9d4e435edb66e6a3278916295ed6cf621072
Takuya Hirose <arin.yaruta@gmail.com>
2015-06-09 12:33:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/no_fate/androidtvapp/test.java
@@ -0,0 +1,9 @@
+package com.example.no_fate.androidtvapp;
+
+/**
+ * Created by takuya on 2015/06/09.
+ */
+public class test {
+    private static final String TAG = test.class.getSimpleName();
+    private final test self = this;
+}
